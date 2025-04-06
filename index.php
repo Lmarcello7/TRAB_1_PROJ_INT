4bd5f8278c248c98c61b76e0d2933b12f8f488c1
Lmarcello7 <lmarcello887@gmail.com>
2025-04-06 20:58:46 -0300

    em andamento

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>To-do List</title>
+	<link rel="stylesheet" href="style/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap3-dialog@1.35.4/dist/css/bootstrap-dialog.min.css">
+</head>
+
+<body>
+	<div class="container-fluid mt-3">
+		<div class="card">
+			<h5 class="card-header text-center">To-do List</h5>
+			<div class="card-body">
+				<div class="row">
+					<div class="col-12">
+						<button type="button" id="btnCadastro" class="btn btn-sm alert-success"><i class="fa fa-plus-square-o"></i> Criar nova tarefa</button>
+					</div>
+				</div>
+				<div class="row mt-2">
+					<!-- TAREFAS EM ANDAMENTO -->
+					<div class="col-5">
+						<div class="card">
+							<h5 class="card-header">Tarefas em andamento <i class="fa fa-angle-down" aria-hidden="true"></i></h5>
+							<div class="card-body d-flex justify-content-center">
+								<div class="card-tarefa card d-inline-block w-75">
+									<div class="card-body">
+										<h5 class="card-title"><input type="checkbox" name="finTask" id="finTask" title="Finalizar Tarefa"> Titulo tarefa</h5>
+										<h6>Descrição da tarefa</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-2"></div>
+					<!-- TAREFAS CONCLUIDAS -->
+					<!-- <div class="col-5">
+						<div class="card">
+							<h5 class="card-header">Tarefas em Finalizadas <i class="fa fa-angle-down" aria-hidden="true"></i></h5>
+							<div class="card-body">
+								<div class="card-tarefa card w-75">
+									<div class="card-body">
+										<h5 class="card-title"><input type="checkbox" name="finTask" id="finTask" title="Finalizar Tarefa"> Titulo tarefa</h5>
+										<h6>Descrição da tarefa</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div> -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap3-dialog@1.35.4/dist/js/bootstrap-dialog.min.js"></script>
+	<script>
+		$(document).ready(function() {
+			$('#btnCadastro').click(function() {
+				BootstrapDialog.show({
+					title: 'Olá!',
+					message: 'Este é um modal criado com Bootstrap Dialog.',
+					buttons: [{
+						label: 'Fechar',
+						action: function(dialogRef) {
+							dialogRef.close();
+						}
+					}]
+				});
+			});
+		});
+	</script>
+</body>
+
+</html>
