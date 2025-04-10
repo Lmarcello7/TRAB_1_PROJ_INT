62fa14df5b234920be7a52f110ac6e6a6adc4c42
Lmarcello7 <lmarcello887@gmail.com>
2025-04-09 22:14:12 -0300

    em andamento

--- js/main.js
+++ js/main.js
@@ -1,26 +1,66 @@
 $(function () {
     $('#btnCadastro').click(function () {
         BootstrapDialog.show({
-            title: '<h4 class="text-dark">Cadastro de Produtos</h4>',
-            id: 'modalCadAtv',
+            title: '<h4 class="text-dark">Criar nova tarefa</h4>',
             message: function (dialog) {
-                var loading = '<div id="divLoad" class="text-center"><i class="fa fa-spinner fa-spin fa-3x" aria-hidden="true"></i><br><b>Carregando</b></div>';
-                return $(`<div>${loading}</div>`).load('../view/modalCad.php');
+                return $(`
+                    <div class="row">
+                        <div class="col-12">
+                            <input type="text" name="nmTarefa" id="nmTarefa" class="form-control form-control-sm" placeholder="Nome da tarefa" />
+                            <small class="aviso d-none text-danger">*Nome Invalido, tente novamente!</small>
+                        </div>
+                    </div>`
+                );
             },
             buttons: [{
-                label: 'Fechar',
+                label: 'Cancelar',
                 cssClass: 'btn-light btn-size-4 mr-02 text-center',
                 action: function (dialog) {
                     dialog.close();
                 }
             },
             {
-                label: 'Salvar',
+                label: 'Criar Tarefa',
                 cssClass: 'btn-success btn-size-4 mr-02 text-center',
                 action: function (dialog) {
-                    
+                    let nmTarefa = $('#nmTarefa').val().trim();
+
+                    if(!nmTarefa) {
+                        $('.aviso').removeClass('d-none');
+                        return false;
+                    }
+
+                    dialog.close();
+                    criarTarefa(nmTarefa);
                 }
             }]
         });
     });
 });
+
+function criarTarefa(nmTarefa)
+{
+    BootstrapDialog.show({
+        title: `<h4 class="text-dark">${nmTarefa}</h4>`,
+        id: 'modalCadAtv',
+        closable: false,
+        message: function (dialog) {
+            var loading = '<div id="divLoad" class="text-center"><i class="fa fa-spinner fa-spin fa-3x" aria-hidden="true"></i><br><b>Carregando</b></div>';
+            return $(`<div>${loading}</div>`).load('view/modalCad.php', {tarefa: nmTarefa});
+        },
+        buttons: [{
+            label: 'Fechar',
+            cssClass: 'btn-light btn-size-4 mr-02 text-center',
+            action: function (dialog) {
+                dialog.close();
+            }
+        },
+        {
+            label: 'Salvar',
+            cssClass: 'btn-success btn-size-4 mr-02 text-center',
+            action: function (dialog) {
+                
+            }
+        }]
+    });
+}
--- view/modalCad.php
+++ view/modalCad.php
@@ -1,4 +1,39 @@
-<?php 
-    sleep(1);
-    echo "AAAAAAAA";
+<?php
+sleep(1); /* PERMITE O JS CARREGAR CORRETAMENTE */
 ?>
+
+<div class="container-fluid">
+	<form id="formInfo">
+		<div class="row">
+			<div class="col-2">
+				<label for="dtVenc">Vencimento</label>
+				<input type="date" name="dtVenc" id="dtVenc" class="form-control form-control-sm text-center">
+			</div>
+			<div class="col-8"></div>
+			<div class="col-2">
+				<label for="prioridade">Prioridade</label>
+				<select name="prioridade" id="prioridade" class="form-control form-control-sm">
+					<option value="B">Baixa</option>
+					<option value="M">Média</option>
+					<option value="A">Alta</option>
+				</select>
+			</div>
+		</div>
+		<div class="row mt-2">
+			<div class="col-12">
+				<label for="obsTarefa">Observações</label>
+				<textarea name="obsTarefa" id="obsTarefa" class="form-control form-control-sm" cols="30" rows="4"></textarea>
+			</div>
+		</div>
+		<div class="row">
+			<div class="form-group col-12">
+				<div class="form-check">
+					<input class="form-check-input" type="checkbox" id="showObs" name="showObs">
+					<label class="form-check-label" for="showObs">
+						Mostrar Observação no card
+					</label>
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
